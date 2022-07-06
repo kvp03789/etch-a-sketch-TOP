7932addd51bb1cda9bbf85b28abaca43259155ee
clay <clay.nichols@gmail.com>
2022-07-06 01:09:28 -0500

    Establish basic grid with Flexbox.

--- scripts.js
+++ scripts.js
@@ -4,17 +4,11 @@ const container = document.querySelector('#container');
 //MAKE GIVEN NUMBER OF DIVS IN ROWS AND COLUMNS
 
 function makeLotsDivs(x, y) {
-    for (let i = 1; i <= y; i++) {
-        var newDivsY = document.createElement('div');
-        newDivsY.className = "verticalDivs";
-        newDivsY.setAttribute("id", `verticalDiv${i}`);
-        container.appendChild(newDivsY);
-            for (let j = 0; j < x; j++) {
-                var newDivsX = document.createElement('div');
-                newDivsX.className = "horizontalDiv";
-                newDivsY.setAttribute("id", `horizontalDiv${j}`)
-                document.querySelector(`#verticalDiv${i}`).appendChild(newDivsX);              
-        }
+    for (let i = 1; i <= (x*y); i++) {
+        var newDivs = document.createElement('div');
+        newDivs.className = "gridOfDivs";
+        container.appendChild(newDivs);
+            
     }
 }
 console.log(makeLotsDivs(16, 16));

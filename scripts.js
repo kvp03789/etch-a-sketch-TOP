8840b14ab0821cbb36d4fff8782d2aa772588b3a
clay <clay.nichols@gmail.com>
2022-07-08 00:35:44 -0500

    Fix color changing event listener. it actually works in the most basic way now! add slider, but still can't get value from it.

--- scripts.js
+++ scripts.js
@@ -4,32 +4,36 @@ const childDivs = container.childNodes;
 
 
 //MAKE GIVEN NUMBER OF DIVS IN ROWS AND COLUMNS
-
 function makeLotsDivs(x, y) {
     for (let i = 1; i <= (x*y); i++) {
         var newDivs = document.createElement('div');
         newDivs.className = "gridOfDivs";
-        container.appendChild(newDivs);
-            
+        newDivs.addEventListener("mouseover", turnWhite)
+        container.appendChild(newDivs);       
     }
 }
 
 //THIS FUNCTION IS CALLED FOR EVENT LISTENER
-
 function turnWhite(event) {
-event.target.classList.add("changeColor");
-}
+    event.target.classList.add("changeColor");
+    }
 
-const childDivs2 = Array.from(childDivs);
-for (let i = 0; i < childDivs2.length; i++) {
-    childDivs2[i].addEventListener("mouseover", turnWhite);
-}
 
-//for(const element of childDivs) {
-//    element.addEventListener("mouseover", turnWhite);
+let slider = document.getElementById("slider");
+let output = document.getElementById("value");
+
+//output.innerHTML = slider.value;
+//console.log(slider.value);
+//slider.oninput = () => {
+//    output.innerHTML = this.value;
 //}
 
 console.log(makeLotsDivs(16, 16));
+
+
+
+
+
 
 
 

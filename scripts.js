55432a3bdd8e98e0e01d42f18224805c7af4dd63
clay <clay.nichols@gmail.com>
2022-07-11 01:15:22 -0500

    Add RGB toggle switch

--- scripts.js
+++ scripts.js
@@ -5,6 +5,8 @@ const childDivs = container.childNodes;
 let slider = document.getElementById("slider");
 let output = document.getElementById("value");
 let sliderValue = slider.value;
+const toggleSwitch = document.getElementById("toggle");
+
 
 
 //MAKE GIVEN NUMBER OF DIVS IN ROWS AND COLUMNS
@@ -12,15 +14,29 @@ function makeLotsDivs(x, y) {
     for (let i = 1; i <= (x*y); i++) {
         var newDivs = document.createElement('div');
         newDivs.className = "gridOfDivs";
+        if (!toggleSwitch.checked) {
         newDivs.addEventListener("mouseover", turnWhite)
+        }
+        else {newDivs.addEventListener("mouseover", turnRGB)};
         container.appendChild(newDivs);      
     }
 }
 
-//THIS FUNCTION IS CALLED FOR EVENT LISTENER
+//WHITE COLOR FOR EVENT LISTENER
 function turnWhite(event) {
     event.target.classList.add("changeColor");
     }
+
+//RGB FOR EVENT LISTENER
+function turnRGB(event) {
+    let r = Math.floor(Math.random()*255);
+    let g = Math.floor(Math.random()*255);
+    let b = Math.floor(Math.random()*255);
+    let rgbValue = `rgb(${r},${g},${b})`;
+    event.target.style.backgroundColor = `${rgbValue}`;
+
+}
+
 
     
 //FUNCTION TO UPDATE VALUE WITH RANGE INPUT
@@ -52,15 +68,25 @@ slider.addEventListener('mouseup', function() {
     container.style.cssText = `grid-template-columns: repeat(${newSliderValue}, 1fr);grid-template-rows: repeat(${newSliderValue}, 1fr);`;
 });
 
-function changeCSS(sliderValue) {
-    let newestSliderValue = slider.value;
-        document.querySelector("#container").style.cssText = `grid-template-column(, 1fr)`;
+//TOGGLE EVENT
+toggleSwitch.addEventListener('change', function() {
+    killChildNodes(container);
+    let newSliderValue = slider.value;
+    console.log(makeLotsDivs(newSliderValue, newSliderValue));
+    container.style.cssText = `grid-template-columns: repeat(${newSliderValue}, 1fr);grid-template-rows: repeat(${newSliderValue}, 1fr);`;
+});
 
-}
+
+
+// function changeCSS(sliderValue) {
+//     let newestSliderValue = slider.value;
+//         document.querySelector("#container").style.cssText = `grid-template-column(, 1fr)`;
+
+// }
 
 
 
-//console.log(makeLotsDivs(updateInput(), updateInput()));
+console.log(makeLotsDivs(updateInput(), updateInput()));
 
 
 

eab431293589d5d33c1b8c3f783980676a8fde47
clay <clay.nichols@gmail.com>
2022-07-11 23:23:17 -0500

    Add some final CSS

--- scripts.js
+++ scripts.js
@@ -15,17 +15,22 @@ function makeLotsDivs(x, y) {
         var newDivs = document.createElement('div');
         newDivs.className = "gridOfDivs";
         if (!toggleSwitch.checked) {
-        newDivs.addEventListener("mouseover", turnWhite)
+            newDivs.addEventListener("mouseover", turnWhite);  
         }
-        else {newDivs.addEventListener("mouseover", turnRGB)};
-        container.appendChild(newDivs);      
+        else if (toggleSwitch.checked) {
+            newDivs.addEventListener("mouseover", turnRGB);  
+        }
+        // else {newDivs.addEventListener('mouseover', (event) => {
+        //     if (event.shiftKey) {event.target.style.backgroundColor = `rgb(0,0,0)`;}
+        // })}
+    container.appendChild(newDivs);
     }
 }
 
 //WHITE COLOR FOR EVENT LISTENER
 function turnWhite(event) {
-    event.target.classList.add("changeColor");
-    }
+    event.target.classList.add("changeColor");}
+    
 
 //RGB FOR EVENT LISTENER
 function turnRGB(event) {
@@ -34,10 +39,13 @@ function turnRGB(event) {
     let b = Math.floor(Math.random()*255);
     let rgbValue = `rgb(${r},${g},${b})`;
     event.target.style.backgroundColor = `${rgbValue}`;
-
 }
 
-
+//CLICK TO ERASE EVENT
+function erase(event) {
+    // event.target.style.removeProperty('background-color');
+    event.target.style.backgroundColor = `rgb(0,0,0)`;
+}
     
 //FUNCTION TO UPDATE VALUE WITH RANGE INPUT
 function updateInput() {
@@ -75,14 +83,6 @@ toggleSwitch.addEventListener('change', function() {
     console.log(makeLotsDivs(newSliderValue, newSliderValue));
     container.style.cssText = `grid-template-columns: repeat(${newSliderValue}, 1fr);grid-template-rows: repeat(${newSliderValue}, 1fr);`;
 });
-
-
-
-// function changeCSS(sliderValue) {
-//     let newestSliderValue = slider.value;
-//         document.querySelector("#container").style.cssText = `grid-template-column(, 1fr)`;
-
-// }
 
 
 
